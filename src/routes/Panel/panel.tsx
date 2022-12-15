dc64c90cf089b4470d1662ea20d04886823fc3f1
Kjell Harald <kjell.kodehode@gmail.com>
2022-12-15 12:50:42 +0100

    not working

--- src/routes/Panel/panel.tsx
+++ src/routes/Panel/panel.tsx
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from "react"
+import './panel.css'
 
 const Panel = ():JSX.Element => {
     const [login, setLogin] = useState({
@@ -8,19 +9,31 @@ const Panel = ():JSX.Element => {
     })
 
     const vdbApi = async (post:string) => {
-        const response:Response = await fetch('', {method: "POST", body: JSON.stringify(post)})
+        const response:Response = await fetch('https://versa-db.com/web/api.php', {
+            method: "POST",
+            mode: "no-cors",
+            headers: {'Content-Type': 'application/json',}, 
+            body: JSON.stringify({webdb: post})
+        })
+        
         const data:string = await response.text()
-    
+        console.log(data)
         if(post.split(' ')[0] === "-auth-only"){
-            const loginCheck = data === "true" ? "" : setLogin({
-                ...login,
-                isLoggedin: !login.isLoggedin
-            })
+            if(data === "true"){
+                console.log(data)
+                setLogin({
+                    ...login,
+                    isLoggedin: !login.isLoggedin
+                })
+            }
         }
     }
 
     const stateHandler = (event:any):void =>  {
         const {value, name} = event.target 
+        if(login.isLoggedin){
+            return
+        }
         setLogin({
             ...login,
             [name]: value
@@ -29,27 +42,39 @@ const Panel = ():JSX.Element => {
     }
 
     const handleLogin = ():void => {
+        const {username, password} = login
+        vdbApi(`-auth-only ${username} ${password}`)
         console.log('Login')
-        console.log(login)
     }
 
     const render:JSX.Element = login.isLoggedin ? (
         <div>
-            <p>Loggedin</p>
+            <select name="command">
+                <option value="-create-user">-create-user</option>
+                <option value="-create-db">-create-db</option>
+                <option value="-db-write">-db-write</option>
+                <option value="-db-write-encoded">-db-write-encoded</option>
+                <option value="-db-search">-db-search</option>
+                <option value="-db-search-encoded">-db-search-encoded</option>
+                <option value="-frag-db">-frag-db</option>
+                <option value="-frag">-frag</option>
+                <option value="-create-frag">-create-frag</option>
+                <option value="-frag-write">-frag-write</option>
+                <option value="-frag-info">-frag-info</option>
+                <option value="-db-storage">-db-storage</option>
+            </select>
         </div>
     )
-    : (
-        <div>
+    : 
+    (
+        <div className="panel-login">
             <input onChange={(event)=> stateHandler(event)} type="text" name="username" placeholder="Username" />
             <input onChange={(event)=> stateHandler(event)} type="password" name="password" placeholder="Password" />
             <button onClick={() => handleLogin()}>Login</button>
         </div>
     )
    
-    console.log(login)
-    return (
-        render
-    )
+    return (render)
 }
 
 export default Panel
